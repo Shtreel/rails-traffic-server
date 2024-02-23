b24c95591d904d85ad38c67e0b40d07849cb5e87
william <williamcwtam@gmail.com>
2024-02-22 22:41:17 -0500

    Added bang operator for credential keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => Rails.application.credentials.gmail_user,
-    :password             => Rails.application.credentials.gmail_password,
+    :user_name            => Rails.application.credentials.gmail_user!,
+    :password             => Rails.application.credentials.gmail_password!,
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
